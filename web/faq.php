48e3504ca48593a905fd91fcb8ac09d33008d83a
malby <malby>
2003-03-21 23:41:01 +0000

    No content - Just testing TortoiseCVS

---
+++ web/faq.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Bamboo.Prevalence - Frequently Asked Questions</title>
+</head>
+<body>
+<table width="100%">
+<tr>
+    <td><A href="http://sourceforge.net"> <IMG src="http://sourceforge.net/sflogo.php?group_id=61693&amp;type=5" width="210" height="62" border="0" alt="SourceForge Logo"></A></td>
+    <td><div align="right"><span align="right">2003-03-22</span></div></td>
+</tr>
+</table>
+
+<h1>Bamboo.Prevalence - Frequently Asked Questions</h1>
+
+<!-- Place Holder -->
+
+Just testing TortoiseCVS
+
+</body>
+</html>
